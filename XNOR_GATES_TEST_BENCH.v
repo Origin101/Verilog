915cb6ec43ba4ac0e97cb8b84ef722f657682f43
Origin101 <89225956+Origin101@users.noreply.github.com>
2021-09-14 06:18:26 +0800

    Create XNOR_GATES_TEST_BENCH.v

---
+++ XNOR_GATES_TEST_BENCH.v
@@ -0,0 +1,16 @@
+`include "XNOR_GATE.v"
+module XNOR;
+reg A, B;wire Y;
+XNOR_GATE Instance0 (Y, A, B);
+initial begin
+    A = 0; B = 0;
+ #1 A = 0; B = 1;
+ #1 A = 1; B = 0;
+ #1 A = 1; B = 1;
+   end
+initial begin
+    $monitor ("%t | A = %d| B = %d| Y = %d", $time, A, B, Y);
+    $dumpfile("dump.vcd");
+    $dumpvars();
+end
+endmodule
